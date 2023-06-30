cd5961725bbb6c53cef5b0f297ca68783c9fa55f
ToxicStar <429771027@qq.com>
2023-06-30 10:31:53 +0800

    调整下载器 增加webgl模式下的组件

---
+++ Assets/MainPackage/Component/InputFieldMobileSupport.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InputFieldMobileSupport : MonoBehaviour
+#if UNITY_WEBGL && !UNITY_EDITOR
+    ,IPointerClickHandler
+#endif
+{
+    public InputField Input;
+
+    public string message;
+
+    private void Awake()
+    {
+        Input = GetComponent<InputField>();
+    }
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern void Prompt(string name, string message, string defaultValue);
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var str =  Input.text;
+        if (string.IsNullOrWhiteSpace(str))
+        {
+            str = message;
+        }
+        Prompt(name, message, str);
+    }
+
+    public void OnPromptOk(string message)
+    {
+        Input.text = message;
+    }
+
+    public void OnPromptCancel()
+    {
+        Input.text = "";
+    }
+#endif
+}

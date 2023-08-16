67ee8cc2bc051bb513dea05473f486a942e50d44
ToxicStar <429771027@qq.com>
2023-08-16 15:17:45 +0800

    添加Chatgpt快捷调用工具 添加空mono

---
+++ Assets/Editor/ChatgptWindow.cs
@@ -0,0 +1,170 @@
+/*********************************************
+ * BFramework
+ * 快捷打开工具
+ * 创建时间：2022/12/28 15:52:23
+ *********************************************/
+using LitJson;
+using MainPackage;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Policy;
+using System.Text;
+using UnityEditor;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Framework
+{
+    /// <summary>
+    /// Chatgpt即时问答工具
+    /// </summary>
+    public class ChatgptWindow : EditorWindow
+    {
+        private static ChatgptWindow _chatgptWindow;
+
+        [MenuItem("BFramework/Chatgpt Tools")]
+        private static void OpenWindow()
+        {
+            var win = CreateWindow<ChatgptWindow>("Chatgpt即时问答工具");
+            win.position = new Rect(100, 100, 600, 400);
+            win.OnInit();
+            _chatgptWindow?.Close();
+            _chatgptWindow = win;
+        }
+
+        public void OnInit()
+        {
+            _openaiSaveKeyPath = Application.persistentDataPath + "/openaikey.txt";
+
+            if (File.Exists(_openaiSaveKeyPath))
+            {
+                _openaiKey = File.ReadAllText(_openaiSaveKeyPath);
+            }
+        }
+
+        private string _question = "";
+        private string _answer = "";
+        private string _openaiKey = "";
+        private string _openaiSaveKeyPath = "";
+        public void OnGUI()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("你的问题：", GUILayout.Width(80f));
+            _question = GUILayout.TextField(_question, GUILayout.Width(440f));
+            if (GUILayout.Button("确定"))
+            {
+                Debug.Log("确定");
+                HttpPost();
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10f);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("回复：", GUILayout.Width(80f));
+            _answer = GUILayout.TextField(_answer, GUILayout.Height(300f));
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10f);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("OpenAI Key：", GUILayout.Width(80f));
+            _openaiKey = GUILayout.TextField(_openaiKey, GUILayout.Width(400f));
+            if (GUILayout.Button("保存"))
+            {
+                if (File.Exists(_openaiSaveKeyPath))
+                {
+                    File.Delete(_openaiSaveKeyPath);
+                }
+                File.WriteAllText(_openaiSaveKeyPath, _openaiKey);
+            }
+            if (GUILayout.Button("打开"))
+            {
+                OpenFileTools.OpenFile(Application.persistentDataPath);
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10f);
+        }
+
+        private UnityWebRequest _webRequest;
+        private string _openaiUrl = "https://api.openai.com/v1/chat/completions";
+        private void HttpPost()
+        {
+            _webRequest = new UnityWebRequest(_openaiUrl, "POST");
+            _webRequest.downloadHandler = new DownloadHandlerBuffer();
+            var json = "{\"model\":\"gpt-4\",\"messages\": [{\"role\": \"user\", \"content\": \"" + _question + "\"}]}";
+            _webRequest.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(json));
+            _webRequest.SetRequestHeader("Content-Type", "application/json");
+            _webRequest.SetRequestHeader("Authorization", "Bearer " + _openaiKey);
+
+            Debug.Log(string.Format("<color=#FFCCFF>url={0}{1}</color>", _openaiUrl, json));
+            //新建一个对象用来暂用协程，用完删除
+            var go = new GameObject("chatgptWindow");
+            var emtpyMono = go.AddComponent<EmtpyMono>();
+            emtpyMono.StartCoroutine(SendRequest(emtpyMono));
+        }
+
+        private IEnumerator SendRequest(EmtpyMono emtpyMono)
+        {
+            _webRequest.timeout = 120;
+            yield return _webRequest.SendWebRequest();
+            if (_webRequest.result == UnityWebRequest.Result.ConnectionError || _webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                //更新错误
+                _answer = _webRequest.error;
+                //打印错误
+                if (!string.IsNullOrWhiteSpace(_answer))
+                {
+                    Debug.Log(_answer);
+                }
+            }
+            else
+            {
+                //更新回复
+                var jsonData = JsonMapper.ToObject<ChatgptMessage>(_webRequest.downloadHandler.text);
+                _answer = jsonData.choices[0].message.content;
+                //打印数据
+                if (!string.IsNullOrWhiteSpace(_answer))
+                {
+                    Debug.Log(string.Format("<color=#FFF11A>{{\"code\":{0},\"data\":{1}}}</color>", _webRequest.responseCode, _answer));
+                }
+            }
+            _webRequest = null;
+            DestroyImmediate(emtpyMono.gameObject);
+        }
+
+        public class ChatgptMessage
+        {
+            public string id;           //唯一标识符
+            public string @object;      //
+            public long created;        //创建时间
+            public string model;        //chatgpt模式
+            public List<Choices> choices;           //
+            public Tokens usage;        //字符统计
+            public class Choices
+            {
+                public int index;                   //消息顺序
+                public ChatMessage message;   //消息列表
+                public string finish_reason;        //
+
+                public class ChatMessage
+                {
+                    public string role;
+                    public string content;
+                }
+            }
+
+            public class Tokens
+            {
+                public int prompt_tokens;           //发送的Token数
+                public int completion_tokens;       //回复的Token数
+                public int total_tokens;            //合计Token数
+            }
+        }
+    }
+
+}

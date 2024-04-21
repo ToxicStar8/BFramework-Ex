c4b0101b90b2bb0c700edf198b3b823774fe4a5b
剧毒星 <429771027@qq.com>
2024-04-21 22:29:48 +0800

    添加游戏暂停UI

---
+++ Assets/GameData/Scripts/UI/Common/UIPause.Design.cs
@@ -0,0 +1,33 @@
+﻿/*********************************************
+ * 自动生成代码，禁止手动修改文件
+ * 脚本名：UIPause.Design.cs
+ * 修改时间：2024/04/21 22:25:01
+ *********************************************/
+
+using Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameData
+{
+    public partial class UIPause
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Framework.ImageEx Img_Bg;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Framework.ButtonEx Btn_Back;
+
+        public override void OnCreate()
+        {
+            rectTransform = gameObject.GetComponent<RectTransform>();
+            Img_Bg = rectTransform.Find("Img_Bg").GetComponent<Framework.ImageEx>();
+			Btn_Back = rectTransform.Find("Btn_Back").GetComponent<Framework.ButtonEx>();
+			
+        }
+    }
+}
---
+++ Assets/GameData/Scripts/UI/Common/UIPause.cs
@@ -0,0 +1,39 @@
+﻿/*********************************************
+ * 
+ * 脚本名：UIPause.cs
+ * 创建时间：2024/04/21 22:23:31
+ *********************************************/
+using Framework;
+using MainPackage;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameData
+{
+    public partial class UIPause : GameUIBase
+    {
+        public override void OnInit()
+        {
+            Btn_Back.AddListener(() =>
+            {
+                Time.timeScale = 1;
+#if UNITY_EDITOR
+                GameEntry.Instance.TimeScale = 1;
+#endif
+                CloseSelf();
+            });
+        }
+
+        public override void OnShow(params object[] args)
+        {
+            
+        }
+
+        public override void OnBeforeDestroy()
+        {
+            
+        }
+    }
+}

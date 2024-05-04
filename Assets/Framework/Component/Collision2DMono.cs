933e6d192e64d558d66a0a682275b62da4892adc
剧毒星 <429771027@qq.com>
2024-05-04 19:08:08 +0800

    添加Collision2DMono.cs

---
+++ Assets/Framework/Component/Collision2DMono.cs
@@ -0,0 +1,33 @@
+﻿/*********************************************
+ * BFramework
+ * 碰撞绑定类
+ * 创建时间：2023/05/10 13:49:23
+ *********************************************/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public class Collision2DMono : MonoBehaviour
+    {
+        public Action<Collision2D> OnTrigger2DEnterCallback;
+        public Action<Collision2D> OnTrigger2DStayCallback;
+        public Action<Collision2D> OnTrigger2DExitCallback;
+
+        //碰撞器器原生方法
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            OnTrigger2DEnterCallback?.Invoke(collision);
+        }
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            OnTrigger2DStayCallback?.Invoke(collision);
+        }
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            OnTrigger2DExitCallback?.Invoke(collision);
+        }
+    }
+}

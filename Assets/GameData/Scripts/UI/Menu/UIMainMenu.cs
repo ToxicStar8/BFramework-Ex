a3b96d9ccad061c6c6b17d1990c5e12cc4c11468
剧毒星 <429771027@qq.com>
2024-06-05 21:46:26 +0800

    写错字

--- Assets/GameData/Scripts/UI/Menu/UIMainMenu.cs
+++ Assets/GameData/Scripts/UI/Menu/UIMainMenu.cs
@@ -24,26 +24,15 @@ namespace GameData
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
-                ShowTips("测试1");
 
-                AddTimer("1", TimerInfo.Create(4, 1000, true, () =>
-                {
-                    Log(E_Log.Log, Time.time.ToString());
-                }));
             }
             if (Input.GetKeyDown(KeyCode.S))
             {
-                RemoveTimer("1");
+
             }
             if (Input.GetKeyDown(KeyCode.D))
             {
-                for (int i = 0; i < 5; i++)
-                {
-                    AddTimer(i.ToString(), TimerInfo.Create(4, 1000, true, () =>
-                    {
-                        Log(E_Log.Log, Time.time.ToString());
-                    }));
-                }
+
             }
         }
 

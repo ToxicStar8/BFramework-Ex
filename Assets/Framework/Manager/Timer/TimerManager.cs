862f91f7b2f41e2976ff34c8c45ce646f49bc0a6
ToxicStar8 <64957222+ToxicStar8@users.noreply.github.com>
2025-09-25 03:48:29 +0800

    定时管理器添加主线程（主要用于UI更新）处理

--- Assets/Framework/Manager/Timer/TimerManager.cs
+++ Assets/Framework/Manager/Timer/TimerManager.cs
@@ -18,6 +18,7 @@ namespace Framework
     public class TimerInfo
     {
         public Action ExecCallback;         //执行回调
+        public Action MainThreadCallback;   //主线程上执行的回调
         public Action<bool> EndCallback;    //结束回调
         public int AllCount;                //执行次数
         public int InviteTime;              //执行间隔时间，毫秒
@@ -26,21 +27,25 @@ namespace Framework
         //定时器管理器里赋值
         public string TimerName;            //定时器名
 
-        public static TimerInfo Create(int allCount, int inviteTime, bool isExecImmed, Action execCallback, Action<bool> endCallback = null)
+        public static TimerInfo Create(int allCount, int inviteTime, bool isExecImmed, Action execCallback = null, Action mainThreadCallback = null, Action<bool> endCallback = null)
         {
             var timerInfo = GameGod.Instance.PoolManager.CreateClassObj<TimerInfo>();
             timerInfo.AllCount = allCount;
             timerInfo.InviteTime = inviteTime;
             timerInfo.IsExecImmed = isExecImmed;
             timerInfo.ExecCallback = execCallback;
+            timerInfo.MainThreadCallback = mainThreadCallback;
             timerInfo.EndCallback = endCallback;
             timerInfo.Cts = new CancellationTokenSource();
             return timerInfo;
         }
+        
         public static void Recycle(TimerInfo timerInfo)
         {
+            timerInfo.Cts?.Dispose();
             timerInfo.Cts = null;
             timerInfo.ExecCallback = null;
+            timerInfo.MainThreadCallback = null;
             timerInfo.EndCallback = null;
             GameGod.Instance.PoolManager.RecycleClassObj(timerInfo);
         }
@@ -57,6 +62,11 @@ namespace Framework
         private Dictionary<string, TimerInfo> _timerInfoDic;
 
 #if !UNITY_WEBGL
+        /// <summary>
+        /// 用于取消定时器线程
+        /// </summary>
+        public CancellationTokenSource _cts;
+
         /// <summary>
         /// 等待开始的队列，如果是列表会发生多线程的资源竞争
         /// </summary>
@@ -81,6 +91,7 @@ namespace Framework
 
             //WEBGL不支持多线程
 #if !UNITY_WEBGL
+            _cts = new();
             _waitStartQueue = new();
             //初始化的时候启动一个线程专门处理计时器
             UniTask.RunOnThreadPool(Loop);
@@ -90,10 +101,25 @@ namespace Framework
 #if !UNITY_WEBGL
         private async UniTask Loop()
         {
-            while (true)
+            try
             {
-                TryExecTimer();
-                await UniTask.Yield();
+                while (true)
+                {
+                    TryExecTimer();
+                    await UniTask.Yield(_cts.Token);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                GameGod.Instance.Log(E_Log.Warring, "定时管理器循环取消");
+            }
+            catch (Exception ex)
+            {
+                GameGod.Instance.Log(E_Log.Error, "定时管理器循环遇到意料之外的错误", ex.ToString());
+            }
+            finally
+            {
+
             }
         }
 
@@ -145,16 +171,15 @@ namespace Framework
         /// </summary>
         private async UniTaskVoid ExecTimer(TimerInfo timerInfo)
         {
-            //判断是否马上执行
-            if (timerInfo.IsExecImmed)
-            {
-                timerInfo.AllCount--;
-                timerInfo.ExecCallback?.Invoke();
-            }
-
-            //如果取消了，则会抛出异常
             try
             {
+                //判断是否马上执行
+                if (timerInfo.IsExecImmed)
+                {
+                    timerInfo.AllCount--;
+                    await ExecuteCallbacks(timerInfo);
+                }
+
                 //初始化一个时间用来计算时间偏移
                 long offsetTime = 0;
                 for (int i = 0; i < timerInfo.AllCount; i++)
@@ -163,8 +188,15 @@ namespace Framework
                     long startTime = TimeUtil.GetNowTimeMilliseconds();
                     //下次延迟时间 = 原来延迟的时间 - 上次偏移的时间        //例：inviteTime = 1000 - 1 = 999
                     int inviteTime = (int)(timerInfo.InviteTime - offsetTime);
+                    
+                    // 确保延迟时间不为负数
+                    if (inviteTime <= 0) inviteTime = 1;
+                    
                     await UniTask.Delay(inviteTime, cancellationToken: timerInfo.Cts.Token);
-                    timerInfo.ExecCallback?.Invoke();
+                    
+                    // 执行回调
+                    await ExecuteCallbacks(timerInfo);
+
                     //新的偏移时间 = 现在时间 - 延迟前的时间 - 间隔时间     //例：offsetTime = 2001 - 1000 - 1000 = 1
                     offsetTime = TimeUtil.GetNowTimeMilliseconds() - startTime - inviteTime;
                 };
@@ -179,11 +211,43 @@ namespace Framework
             }
             finally
             {
+                // 确保清理工作在主线程执行
+                await UniTask.SwitchToMainThread();
                 //不管是时间到了还是主动取消，都会在这里进行回收处理
                 timerInfo.EndCallback?.Invoke(timerInfo.Cts.IsCancellationRequested);
                 _timerInfoDic.Remove(timerInfo.TimerName);
                 TimerInfo.Recycle(timerInfo);
                 GameGod.Instance.Log(E_Log.Framework, "定时器回收", timerInfo.TimerName);
+            }
+        }
+
+        /// <summary>
+        /// 执行回调方法（优化线程切换逻辑）
+        /// </summary>
+        private async UniTask ExecuteCallbacks(TimerInfo timerInfo)
+        {
+            // 如果都有回调，优化线程切换
+            if (timerInfo.ExecCallback != null && timerInfo.MainThreadCallback != null)
+            {
+                // 先在后台线程执行计算回调
+                await UniTask.SwitchToThreadPool();
+                timerInfo.ExecCallback.Invoke();
+                
+                // 然后切换到主线程执行UI回调
+                await UniTask.SwitchToMainThread();
+                timerInfo.MainThreadCallback.Invoke();
+            }
+            // 只有后台回调
+            else if (timerInfo.ExecCallback != null)
+            {
+                await UniTask.SwitchToThreadPool();
+                timerInfo.ExecCallback.Invoke();
+            }
+            // 只有主线程回调
+            else if (timerInfo.MainThreadCallback != null)
+            {
+                await UniTask.SwitchToMainThread();
+                timerInfo.MainThreadCallback.Invoke();
             }
         }
 
@@ -222,6 +286,13 @@ namespace Framework
             }
             _timerInfoDic.Clear();
             _timerInfoDic = null;
+
+#if !UNITY_WEBGL
+            //取消定时器线程
+            _cts?.Cancel();
+            _cts?.Dispose();
+            _cts = null;
+#endif
         }
     }
 }

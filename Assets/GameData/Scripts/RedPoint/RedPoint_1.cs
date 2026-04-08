4fc790433a4b00ab89948964462daa2d7e27393a
ToxicStar <toxicstar996@qq.com>
2026-04-08 23:52:25 +0800

    添加基本红点

---
+++ Assets/GameData/Scripts/RedPoint/RedPoint_1.cs
@@ -0,0 +1,19 @@
+/*********************************************
+ * 
+ * 红点1
+ * 创建时间：2025/01/03 21:55:23
+ *********************************************/
+using Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameData
+{
+    /// <summary>
+    /// 红点
+    /// </summary>
+    public class RedPoint_1 : RedPointBase
+    {
+
+    }
+}
